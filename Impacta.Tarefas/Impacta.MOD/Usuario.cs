0237e62e61a1a0faa214159350630368186bd41b
Marcio R <marcioprogramer@gmail.com>
2019-08-16 01:18:08 -0300

    Autenticacao sessao

--- Impacta.Tarefas/Impacta.MOD/Usuario.cs
+++ Impacta.Tarefas/Impacta.MOD/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Impacta.MOD
 {
 	public class Usuario
 	{
+        [Required(ErrorMessage ="Nome do usuario é obrigatório")]
 		public string Username { get; set; }
 		public string Password { get; set; }
 	}
